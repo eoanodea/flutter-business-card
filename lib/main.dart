4e80107061582cd43c013c9aff255383bce950b4
Eoan O Dea <odeaeoan@gmail.com>
2020-10-19 13:32:27 +0200

    Added email and phone boxes with icons

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,57 @@ void main() {
                 color: Colors.teal[100],
                 fontWeight: FontWeight.normal,
               ),
-            )
+            ),
+            Container(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(
+                vertical: 10.0,
+                horizontal: 25.0,
+              ),
+              padding: EdgeInsets.all(10.0),
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.phone,
+                    color: Colors.teal.shade900,
+                  ),
+                  SizedBox(width: 10.0),
+                  Text(
+                    "+353 86 888 3333",
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(
+                vertical: 10.0,
+                horizontal: 25.0,
+              ),
+              padding: EdgeInsets.all(10.0),
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.mail,
+                    color: Colors.teal.shade900,
+                  ),
+                  SizedBox(width: 10.0),
+                  Text(
+                    "hello@eoan.ie",
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         )),
       ),
