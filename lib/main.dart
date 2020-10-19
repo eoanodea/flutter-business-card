0cf72293c860e6072cafb27adba906aacbdafac6
Eoan O Dea <odeaeoan@gmail.com>
2020-10-19 13:20:49 +0200

    Added custom image, custom font and Column containing avatar and text to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,34 @@ void main() {
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+            child: Column(
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage(
+                'images/logo.png',
+              ),
+            ),
+            Text(
+              'Eoan O\'Dea',
+              style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w900),
+            ),
+            Text(
+              'FULL STACK ENGINEER',
+              style: TextStyle(
+                fontFamily: 'SourceSansPro',
+                fontSize: 20.0,
+                color: Colors.teal[100],
+                fontWeight: FontWeight.normal,
+              ),
+            )
+          ],
+        )),
       ),
     ),
   );
