cead6824361a2a6af63d287ae3591a5d6a2e111e
Eoan O Dea <odeaeoan@gmail.com>
2020-10-19 13:43:48 +0200

    Replaced list rows with cards with listtiles

--- lib/main.dart
+++ lib/main.dart
@@ -6,71 +6,43 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-            child: Column(
-          children: <Widget>[
-            CircleAvatar(
-              radius: 50.0,
-              backgroundImage: AssetImage(
-                'images/logo.png',
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage(
+                  'images/logo.png',
+                ),
               ),
-            ),
-            Text(
-              'Eoan O\'Dea',
-              style: TextStyle(
+              Text(
+                'Eoan O\'Dea',
+                style: TextStyle(
+                    fontFamily: 'SourceSansPro',
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w900),
+              ),
+              Text(
+                'FULL STACK ENGINEER',
+                style: TextStyle(
                   fontFamily: 'SourceSansPro',
-                  fontSize: 40.0,
-                  color: Colors.white,
-                  fontWeight: FontWeight.w900),
-            ),
-            Text(
-              'FULL STACK ENGINEER',
-              style: TextStyle(
-                fontFamily: 'SourceSansPro',
-                fontSize: 20.0,
-                color: Colors.teal[100],
-                fontWeight: FontWeight.normal,
+                  fontSize: 20.0,
+                  color: Colors.teal[100],
+                  fontWeight: FontWeight.normal,
+                ),
               ),
-            ),
-            Container(
-              color: Colors.white,
-              margin: EdgeInsets.symmetric(
-                vertical: 10.0,
-                horizontal: 25.0,
-              ),
-              padding: EdgeInsets.all(10.0),
-              child: Row(
-                children: <Widget>[
-                  Icon(
-                    Icons.phone,
-                    color: Colors.teal.shade900,
-                  ),
-                  SizedBox(width: 10.0),
-                  Text(
-                    "+353 86 888 3333",
-                    style: TextStyle(
-                      color: Colors.teal.shade900,
-                      fontFamily: 'SourceSansPro',
-                      fontSize: 20.0,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              color: Colors.white,
-              margin: EdgeInsets.symmetric(
-                vertical: 10.0,
-                horizontal: 25.0,
-              ),
-              padding: EdgeInsets.all(10.0),
-              child: Row(
-                children: <Widget>[
-                  Icon(
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
                     Icons.mail,
                     color: Colors.teal.shade900,
                   ),
-                  SizedBox(width: 10.0),
-                  Text(
+                  title: Text(
                     "hello@eoan.ie",
                     style: TextStyle(
                       color: Colors.teal.shade900,
@@ -78,11 +50,32 @@ void main() {
                       fontSize: 20.0,
                     ),
                   ),
-                ],
+                ),
               ),
-            ),
-          ],
-        )),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal.shade900,
+                  ),
+                  title: Text(
+                    "+353 86 333 8888",
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     ),
   );
